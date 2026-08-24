49eabc7eefdffc4789bd61e33b46f9ffe0eda1fa
keith1101 <anhhuybui2006@gmail.com>
2026-08-24 21:09:41 +0700

    refactor codebase

---
+++ apps/be/auth/auth.service.ts
@@ -0,0 +1,159 @@
+import { ConflictException, ForbiddenException, Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from '../prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { ChangePasswordDto } from './dto/change-password.dto';
+
+
+@Injectable()
+export class AuthService {
+    constructor (
+        private readonly prisma: PrismaService,
+        private readonly jwtService: JwtService,
+    ) {}
+
+    async register(registerDto : RegisterDto) {
+        const { email, password, fullName } = registerDto;
+
+        const existingUser = await this.prisma.user.findUnique({
+            where: { email },
+        });
+
+        if (existingUser) {
+            throw new ConflictException('Email already exists');
+        }
+
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        const user = await this.prisma.user.create ({
+            data: {
+                email,
+                passwordHash,
+                fullName,
+            },
+            select: {
+                id: true,
+                email: true,
+                fullName: true,
+                role: true,
+                isActive: true,
+                isPro: true,
+                proExpiresAt: true,
+                createdAt: true,
+                updatedAt: true,
+            },
+        });
+        return user;
+    }
+
+    async login(loginDto : LoginDto) {
+        const { email, password } = loginDto;
+
+        const user = await this.prisma.user.findUnique({
+            where: { email }
+        });
+
+        if (!user) {
+            throw new UnauthorizedException('Invalid email or password'); //exception
+        }
+
+        if (user.isActive === false) {
+            throw new ForbiddenException('Account is locked');
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.passwordHash);
+        
+        if (!isPasswordValid) {
+            throw new UnauthorizedException('Invalid email or password');   
+        }
+
+        const payload = {
+            sub: user.id,
+            email: user.email,
+            role: user.role
+        }
+        
+        const accessToken = await this.jwtService.signAsync(payload);
+        return {
+            accessToken,
+            user: {
+                id: user.id,
+                email: user.email,
+                fullName: user.fullName,
+                role: user.role,
+                isActive: user.isActive,
+                isPro: user.isPro,
+                proExpiresAt: user.proExpiresAt,
+            },
+        };  
+    }
+
+    async getMe(userId: number) {
+
+        const user = await this.prisma.user.findUnique ({
+            where: {
+                id: userId,
+            },
+            select: {
+                id: true,
+                email: true,
+                fullName: true,
+                role: true,
+                isActive: true,
+                isPro: true,
+                proExpiresAt: true,
+                createdAt: true,
+                updatedAt: true,
+            }
+        })
+
+        if (!user) {
+            throw new UnauthorizedException('User not found!');
+        }
+        return user;
+    }
+
+    async changePassword (
+        userId: number,
+        changePasswordDto: ChangePasswordDto,
+    ) {
+        const { oldPassword, newPassword } = changePasswordDto;
+
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: userId,
+            },
+        });
+
+        if(!user) {
+            throw new UnauthorizedException('User not found');
+        }
+
+        const isOldPasswordValid = await bcrypt.compare(
+            oldPassword,
+            user.passwordHash,
+        );
+
+        if (!isOldPasswordValid) {
+            throw new UnauthorizedException('Old password is incorrect');
+        }
+
+        const newPasswordHash = await bcrypt.hash(newPassword, 10);
+        await this.prisma.user.update({
+            where: {
+                id: userId,
+            },
+            data: {
+                passwordHash: newPasswordHash,
+            },
+        });
+
+        return {
+            message: 'Password changed successfully',
+        };
+
+    }
+
+}
---
+++ apps/be/subjects/dto/create-subject.dto.ts
@@ -0,0 +1,17 @@
+import { IsNotEmpty, IsString, IsOptional } from "class-validator";
+
+export class CreateSubjectDto {
+
+    @IsString()
+    @IsNotEmpty()
+    name!: string;
+
+    @IsString()
+    @IsNotEmpty()
+    code!: string;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+}
